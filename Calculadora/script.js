dcf2c08f833ec3c5e795154196f3dc2de1c2e6da
Davies Araujo <daviesaraujo@hotmail.com>
2022-02-05 23:47:30 -0300

    visor colocando divisor de milhar

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -2,9 +2,76 @@
 const q = (elemento) => document.querySelector(elemento);
 const qA = function(elemento){ return document.querySelectorAll(elemento)};
 
+//Clicks 
+onload = () => {
+    q('#btn-1').onclick = () => digito(1);
+    q('#btn-2').onclick = () => digito(2);
+    q('#btn-3').onclick = () => digito(3);
+    q('#btn-4').onclick = () => digito(4);
+    q('#btn-5').onclick = () => digito(5);
+    q('#btn-6').onclick = () => digito(6);
+    q('#btn-7').onclick = () => digito(7);
+    q('#btn-8').onclick = () => digito(8);
+    q('#btn-9').onclick = () => digito(9);
+    q('#btn-0').onclick = () => digito(0);
+    q('#btn-virgula').onclick = virgula;
+    q('#btn-AC').onclick = limpa
+    /*
+    q('#btn-').onclick = () => 
+    */
+}
 
+//variaveis que armazenam os valores/operadores e o estado da calculadora
+let valorDoVisor = '0';
+let ehNovoNumero = true; // é um novo valor ? 
 
-//relógio + data da ccalculadora
+//atualiza os numeros do visor
+const attVisor = () => {
+    let [inteiro, decimal] = valorDoVisor.split(',');
+    let v = '';
+    let c = 0;
+    
+    //laço for me retornando a quantidade de numeros inteiros inputados
+    for (let i = inteiro.length - 1; i >= 0; i--){
+        //if para acrescenter o ponto a cada 3 caracteres 
+        if(++c > 3){
+            v = "." + v;
+            c = 1;
+        }
+        v = inteiro[i] + v;
+    }
+    //para nao retornar undefined
+    v = v + (decimal ? ',' + decimal : '');
+    q('.input--area').innerText = v;
+}
+
+//pega os clicks
+const digito = (n) => {
+    if (ehNovoNumero){
+        valorDoVisor = '' + n;
+        ehNovoNumero = false;
+    } else valorDoVisor += n;
+    attVisor();
+    efeitoClick(n);
+}
+
+//só pode uma virgula
+const virgula = () => {
+    if (ehNovoNumero){
+        valorDoVisor = '0,';
+        ehNovoNumero = false;
+    } else if(valorDoVisor.indexOf(',') == -1) valorDoVisor += ',';
+    attVisor();
+}
+
+// botão AC All Clear 
+const limpa = () => {
+    ehNovoNumero = true;
+    valorDoVisor = '0';
+    attVisor();
+}
+
+//relógio + data da calculadora
 function exibirDataNoVisor(){
     let data = new Date();
     let dia = data.getDate();
@@ -24,8 +91,13 @@ function exibirDataNoVisor(){
 
     let ligado = true; //*****************
     if (ligado){
-        q('.visor .date').innerHTML = ` ${diaSemana} - ${dia}/${mes}/${ano} - ${hora}:${minutos}:${segundos}`;
+        q('.visor .date').innerHTML = `<u> ${diaSemana} - ${dia}/${mes}/${ano} - ${hora}:${minutos}:${segundos} </u>`;
     } else{
         q('.visor .date').innerHTML = '';
     }
+}
+
+//efeito de click
+const efeitoClick = (n) => {
+ 
 }
