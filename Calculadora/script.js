07152b0bf777ee800c3b831f5a2dfc16888c37a6
DTI Fundacao <davies.araujo@fundacaojuquery.org.br>
2022-02-07 09:17:40 -0300

    Adicionando evento no Botão de Power

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,6 +1,12 @@
 //vars
 const q = (elemento) => document.querySelector(elemento);
 const qA = function(elemento){ return document.querySelectorAll(elemento)};
+let estaLigado = false;
+
+q('#btn-Power').addEventListener('click', () => {
+    powerOnOff();
+    
+});
 
 //Clicks 
 onload = () => {
@@ -24,64 +30,91 @@ onload = () => {
 }
 
 //variaveis que armazenam os valores/operadores e o estado da calculadora
-let sValor = '0';
+let sValor = '';
 let ehNovoNumero = true; // é um novo valor ? 
 let valorAnterior = 0;
 let operacaoPendente = null;
 
+const powerOnOff = () => {
+    if (estaLigado) {
+        exibirDataNoVisor();
+        attVisor();
+        estaLigado = false;
+    } else {
+        sValor = '';
+        ehNovoNumero = true; // é um novo valor ? 
+        valorAnterior = 0;
+        operacaoPendente = null;
+        exibirDataNoVisor();
+        attVisor();
+        estaLigado = true;
+    }
+}
+
 //atualiza os numeros do visor
 const attVisor = () => {
-    let [parteInteira, parteDecimal] = sValor.split(',');
     let v = '';
-    c = 0;
-    
-    //laço for me retornando a quantidade de numeros inteiros inputados
-    for (let i = parteInteira.length - 1; i >= 0; i--){
-        //if para acrescenter o ponto a cada 3 caracteres 
-        if(++c > 3){
-            v = "." + v; //BUG = -.125
-            c = 1;
-        }
-
-        // resolvendo BUG visor exibe = -.123
-        if (parteInteira[0] == "-") {
-            v = parteInteira[i] + v;
-            v = v.replace('-.' , '-');
-        }
-        else v = parteInteira[i] + v;
+    if (estaLigado){
+        let [parteInteira, parteDecimal] = sValor.split(',');
+        c = 0;
         
+        //laço for me retornando a quantidade de numeros inteiros inputados
+        for (let i = parteInteira.length - 1; i >= 0; i--){
+            //if para acrescenter o ponto a cada 3 caracteres 
+            if(++c > 3){
+                v = "." + v; //BUG = -.125
+                c = 1;
+            }
+    
+            // resolvendo BUG visor exibe = -.123
+            if (parteInteira[0] == "-") {
+                v = parteInteira[i] + v;
+                v = v.replace('-.' , '-');
+            }
+            else v = parteInteira[i] + v;
+            
+        }
+        //para nao retornar undefined
+        v = v + (parteDecimal ? ',' + parteDecimal : '');
+        q('.input--area').innerText = v;
+    } else {
+        v = '';
+        q('.input--area').innerText = v;
     }
-    //para nao retornar undefined
-    v = v + (parteDecimal ? ',' + parteDecimal : '');
-    q('.input--area').innerText = v;
 }
 
 //pega os clicks
 const digito = (n) => {
-    if (ehNovoNumero){
-        sValor = '' + n;
-        ehNovoNumero = false;
-    } else sValor += n;
-    attVisor();
-    efeitoClick(n);
+    if (estaLigado){
+        if (ehNovoNumero){
+            sValor = '' + n;
+            ehNovoNumero = false;
+        } else sValor += n;
+        attVisor();
+        efeitoClick(n);
+    }
 }
 
 //só pode uma virgula
 const virgula = () => {
-    if (ehNovoNumero){
-        sValor = '0,';
-        ehNovoNumero = false;
-    } else if(sValor.indexOf(',') == -1) sValor += ',';
-    attVisor();
+    if (estaLigado){
+        if (ehNovoNumero){
+            sValor = '0,';
+            ehNovoNumero = false;
+        } else if(sValor.indexOf(',') == -1) sValor += ',';
+        attVisor();
+    }
 }
 
 // botão AC All Clear 
 const limpa = () => {
-    ehNovoNumero = true;
-    sValor = '0';
-    valorAnterior = 0;
-    operacaoPendente = null;
-    attVisor();
+    if (estaLigado){
+        ehNovoNumero = true;
+        sValor = '';
+        valorAnterior = 0;
+        operacaoPendente = null;
+        attVisor();
+    }
 }
 
 //converte string para Float
@@ -89,35 +122,39 @@ const valorAtual = () => parseFloat(sValor.replace(',', '.'));
 
 // faz os cálculos
 const operador = (op) => {
-    calcula();
-    valorAnterior = valorAtual();
-    operacaoPendente = op;
-    ehNovoNumero = true;
+    if (estaLigado){
+        calcula();
+        valorAnterior = valorAtual();
+        operacaoPendente = op;
+        ehNovoNumero = true;
+    }
 }
 
 const calcula = () => {
-    if (operacaoPendente != null){
-        let resultado;
-        switch (operacaoPendente){
-            case '+': 
-                resultado = valorAnterior + valorAtual(); 
-                break;
-            case '-':   
-                resultado = valorAnterior - valorAtual(); 
-                break;
-            case '*':   
-                resultado = valorAnterior * valorAtual(); 
-                break;
-            case '/':   
-                resultado = valorAnterior / valorAtual(); 
-                break;
+    if (estaLigado){
+        if (operacaoPendente != null){
+            let resultado;
+            switch (operacaoPendente){
+                case '+': 
+                    resultado = valorAnterior + valorAtual(); 
+                    break;
+                case '-':   
+                    resultado = valorAnterior - valorAtual(); 
+                    break;
+                case '*':   
+                    resultado = valorAnterior * valorAtual(); 
+                    break;
+                case '/':   
+                    resultado = valorAnterior / valorAtual(); 
+                    break;
+            }
+            sValor = resultado.toString().replace('.',',');
         }
-        sValor = resultado.toString().replace('.',',');
+        ehNovoNumero = true; 
+        valorAnterior = 0;
+        operacaoPendente = null;
+        attVisor();
     }
-    ehNovoNumero = true; 
-    valorAnterior = 0;
-    operacaoPendente = null;
-    attVisor();
 }
 
 //relógio + data da calculadora
@@ -138,9 +175,8 @@ function exibirDataNoVisor(){
     if(minutos < 10) { minutos =`0${minutos}`};
     if(segundos < 10) { segundos =`0${segundos}`};
 
-    let ligado = true; //*****************
-    if (ligado){
-        q('.visor .date').innerHTML = `<u> ${diaSemana} - ${dia}/${mes}/${ano} - ${hora}:${minutos}:${segundos} </u>`;
+    if (!estaLigado){
+        q('.visor .date').innerHTML = `${diaSemana} - ${dia}/${mes}/${ano} - ${hora}:${minutos}:${segundos}`;
     } else{
         q('.visor .date').innerHTML = '';
     }
@@ -150,3 +186,9 @@ function exibirDataNoVisor(){
 const efeitoClick = (n) => {
  
 }
+
+/* TAREFAS:
+add efeito de click;
+Adicionar evento ao botão Power on/off;
+arrumar função q exibe data no visor;
+*/
