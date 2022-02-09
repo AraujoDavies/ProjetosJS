46ddf9cf38b9c90e28a404693721e803c322a908
Davies Araujo <daviesaraujo@hotmail.com>
2022-02-08 21:28:59 -0300

    adicionando eventos de Play/Pause e variavel com nome do jogador

--- JogoEstouraBola/script.js
+++ JogoEstouraBola/script.js
@@ -1,12 +1,33 @@
+//var
+let playerName = '';
+
+//events
+document.querySelector('#start--game').addEventListener('click', (e)=>{
+    e.target.style.display = 'none';
+    getPlayer();
+    alert('Para encerrar o game tecle Space')
+    start();
+})
+
+document.body.addEventListener('keyup', (t) => {
+    let teclou = t.code
+    if (teclou == 'Space'){
+        pauseGame();
+    }
+})
+
+
+//functions 
 const addBalao = () => {
     let bola = document.createElement('div');
     bola.setAttribute('class', 'bola');
+    let gameArea = document.querySelector('.tela--game');
 
-    let widthUser = window.screen.width - 100; //menos largura da bola * 2
-    let heightUser = window.screen.height - 240; //menos altura da bola *4
+    let widthGame = gameArea.clientWidth - 50; //menos largura da bola
+    let heightGame = gameArea.clientHeight - 60; //menos altura da bola 
 
-    let p1 = Math.floor(Math.random() * widthUser);
-    let p2 = Math.floor(Math.random() * heightUser);
+    let p1 = Math.floor(Math.random() * widthGame + gameArea.offsetLeft);
+    let p2 = Math.floor(Math.random() * heightGame);
 
     let cor = ['red', 'green', 'yellow', 'blue' ,'orange', 'pink'];
     let iCor = Math.floor(Math.random() * 6)
@@ -15,15 +36,22 @@ const addBalao = () => {
     bola.setAttribute('style', 'left: ' + p1 +'px; top: ' + p2 + 'px;');
     bola.setAttribute('onclick' , 'estourar(this)');
     
-    document.body.append(bola);
+    gameArea.append(bola);
     
 }
 
-
 const estourar = (obj) => {
-    document.body.removeChild(obj);
+    document.querySelector('.tela--game').removeChild(obj);
 }
 
 const start = () => {
-    setInterval(addBalao, 1000);
+    setInterval(addBalao, 100);
+}
+
+const getPlayer = () => {
+    playerName = prompt(" Como é o seu nome ?")
+}
+
+const pauseGame = () => {
+    alert('game pausado')
 }
